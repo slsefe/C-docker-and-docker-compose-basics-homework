9b4b92a21e851b1c81db5cdc95cc618fa92d16c1
Yin Bai <17693374267@163.com>
2021-03-11 12:47:53 +0800

    [baiyin] feat: complete hello api from alice.

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/Controller.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/Controller.java
@@ -1,2 +1,20 @@
-package com.thoughtworks.capability.gtb.alice;public class Service {
+package com.thoughtworks.capability.gtb.alice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String hello() {
+        RestTemplate template = new RestTemplate();
+        String s = "http://app-bob:8081/hello";
+        URI uri = URI.create(s);
+        ResponseEntity<String> response = template.getForEntity(uri, String.class);
+        return response.getBody();
+    }
 }
